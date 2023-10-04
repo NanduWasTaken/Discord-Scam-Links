778f3011d0d7ecd7b718fb2683e7c52e261303b4
Nandu <nanduwastaken@gmail.com>
2023-10-04 20:38:08 +0530

    Update sort.js

--- sort.js
+++ sort.js
@@ -1,6 +1,31 @@
 const fs = require('fs');
 
 const inputFile = './domain-list.txt';
+
+
+
+const fetch = require('node-fetch');
+const url = 'https://raw.githubusercontent.com/nikolaischunk/discord-phishing-links/main/txt/domain-list.txt';
+const filePath = inputFile;
+
+fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.text();
+  })
+  .then(data => {
+    // Write the fetched data to the pre-existing file
+    fs.writeFileSync(filePath, data);
+
+    console.log(`File ${filePath} has been updated with the fetched content.`);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+
 const outputFile = './domains.json';
 
 fs.readFile(inputFile, 'utf8', (err, data) => {
